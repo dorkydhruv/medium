fe5ae34eb0539f91247b51149de1e8078dc13d53
dorkydhruv <thedhruvsharma@gmail.com>
2024-06-14 23:27:56 +0530

    frontend job

--- frontend/src/components/AppBar.tsx
+++ frontend/src/components/AppBar.tsx
@@ -11,6 +11,7 @@ export const AppBar = ()=>{
     </div>
 }
 
+
 function Avatar({
     authorName
 } : {authorName: string}){
--- frontend/src/components/Auth.tsx
+++ frontend/src/components/Auth.tsx
@@ -89,11 +89,16 @@ function LabeledInput({
 interface SubmitButtonProps {
     text: string;
     onClick: ()=>void;
-
 }
 
 function SubmitButton({text, onClick}: SubmitButtonProps){
     return (
         <button onClick={onClick} type="button" className="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100">{text}</button>
+    )
+}
+
+function AuthPage(){
+    return (
+        <Auth type={"signup"}/>
     )
 }
